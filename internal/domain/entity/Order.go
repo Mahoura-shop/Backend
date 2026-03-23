b2375d736f816b4c6eb48bfb69444bf039dd3fb1
AminFiroozi <afiroozi007@gmail.com>
2026-03-23 19:48:17 +0330

    feat: add Order entity

---
+++ internal/domain/entity/Order.go
@@ -0,0 +1,11 @@
+package entity
+
+import (
+	"github.com/Mahoura-shop/Backend/internal/infrastructure/database"
+)
+
+type Order struct {
+	database.Model
+	CartID uint
+	Cart   Cart `gorm:"foreignKey:CartID"`
+}
