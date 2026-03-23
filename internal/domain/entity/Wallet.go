3e889eb81a167bce1872d7aab4eb5deedfd5466e
AminFiroozi <afiroozi007@gmail.com>
2026-03-23 19:26:00 +0330

    feat: add Wallet entity

---
+++ internal/domain/entity/Wallet.go
@@ -0,0 +1,12 @@
+package entity
+
+import (
+	"github.com/Mahoura-shop/Backend/internal/infrastructure/database"
+)
+
+type Wallet struct {
+	database.Model
+	Balance   uint `gorm:"type:int"`
+	UserID    uint
+	User      User `gorm:"foreignKey:UserID"`
+}
